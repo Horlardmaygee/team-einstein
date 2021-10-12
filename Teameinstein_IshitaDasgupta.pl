5a3f8215d56f8aa9e1e714fe155a12127ac228c5
Ishita Dasgupta <84772358+ishitadasgupta@users.noreply.github.com>
2021-10-13 01:26:39 +0530

    Add files via upload

---
+++ Teameinstein_IshitaDasgupta.pl
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+#printing of the details as asked by hackbio
+
+print "Name : Ishita Dasgupta\n";
+print "Email id : ishita2798\@gmail.com\n";
+print "Slack username : \@IshitaDasgupta\n";
+print "Biostack : Molecular Modelling, Drug Development, Data Analytics.\n";
