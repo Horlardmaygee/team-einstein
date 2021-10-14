4f48386d6d4b23b01503a0b45e339fedf28591cd
Eberelouis <92266134+Eberelouis@users.noreply.github.com>
2021-10-14 14:46:57 +0100

    Add files via upload

---
+++ TeamEinstein_Ebere.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  fmt.Println("Hello World")
+    fmt.Println("my name: Ebere")
+    fmt.Println("email: emerci14@gmail.com")
+    fmt.Println("Discord: is @ebere")
+    fmt.Println("Biostack: Data Analysis")
+    fmt.Println("stack: is @ebere")
+}
