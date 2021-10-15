9e129db34a7737f7664a3f93d87b489b798285d8
Solo-prist <92265920+Solo-prist@users.noreply.github.com>
2021-10-15 17:04:04 +0300

    Update Teameinstein_prissא.R

--- Teameinstein_prissא.R
+++ Teameinstein_prissא.R
@@ -1,1 +1,5 @@
-a <- c("solomon" "suoguche@gmail.com" "priss" "genomics")
+name  <- ("solomon")
+email <- ("suoguche@gmail.com")
+slack <- ("@priss")
+biost <- ("Genomics")
+cat (name, email, slack, biosta)
