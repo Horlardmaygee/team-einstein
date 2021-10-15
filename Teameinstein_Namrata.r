1a37b7d3f6264b871c77696c722370b1c9f19155
Namrata2801 <92246471+Namrata2801@users.noreply.github.com>
2021-10-15 15:14:58 +0530

    Update Teameinstein_Namrata.r

--- Teameinstein_Namrata.r
+++ Teameinstein_Namrata.r
@@ -1,6 +1,6 @@
 # HackBio Task1
-a <- ("Name-Namrata,")
-b <- ("Email id-namrata.7838@gmail.com,")
-c <- ("Slack username-Namrata,")
-d <- ("Biostack- Genomics")
+a <- ("Namrata")
+b <- ("namrata.7838@gmail.com")
+c <- ("Namrata")
+d <- ("Genomics")
 cat (a, b, c, d)
