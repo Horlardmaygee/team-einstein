5fff31a989e3825a1391a1bff66d29d171f60d41
kay <79715597+kay1304@users.noreply.github.com>
2021-10-15 13:17:02 -0400

    Add files via upload
    modified script

---
+++ teameinstein_kayode.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ojediran Kayode");
+    printf(" ");
+    printf("kayojediran@gmail.com");
+    printf(" ");
+    printf("@kay_ddon");
+    printf(" ");
+    printf("Genomics");
+    
+
+    return 0;
+}
