b8bfae87b57fe5a9a6028d107dbe7829f072d373
Ishita Dasgupta <84772358+ishitadasgupta@users.noreply.github.com>
2021-10-15 20:32:37 +0530

    Update Teameinstein_IshitaDasgupta.pl

--- Teameinstein_IshitaDasgupta.pl
+++ Teameinstein_IshitaDasgupta.pl
@@ -2,4 +2,4 @@ use strict;
 use warnings;
 
 #printing the required information
-print ("Ishita Dasgupta" , "\t ishita2798\@gmail.com" , "\t \@IshitaDasgupta" , "\t Molecular Modelling, Drug Development, Data Analytics.\n");
+print ("Ishita Dasgupta" , "\t ishita2798\@gmail.com" , "\t \@IshitaDasgupta" , "\tData Analytics.\n");
