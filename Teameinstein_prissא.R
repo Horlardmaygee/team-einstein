9e22c27c8ed1684d949dca20e091fdbbfd9e829a
Solo-prist <92265920+Solo-prist@users.noreply.github.com>
2021-10-15 10:46:55 +0300

    Update Teameinstein_prissא.R

--- Teameinstein_prissא.R
+++ Teameinstein_prissא.R
@@ -1,8 +1,1 @@
-#Header <- c ("Full name", "Email address", "Discord_username", "Biostack")
-#Entry  <- c ("Solomon Oguche", "suoguche@gmail.com", "prissא", "Drug Discovery & Genomics")
-#Full   <- cbind(Header, Entry)
-#print(Full)
-
-sol_hac <-  c("solomon", "suoguche@gmail.com", "priss", "genomics")
-
-        cat(paste(sol_hac, collaspe=','))
+a <- c("solomon", "suoguche@gmail.com", "priss", "genomics")
