211fd37dbf89d9b14a9a106e88e146427cc541fe
Ishita Dasgupta <84772358+ishitadasgupta@users.noreply.github.com>
2021-10-15 20:01:39 +0530

    Delete Teameinstein_IshitaDasgupta.pl

--- Teameinstein_IshitaDasgupta.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-
-#printing of the details as asked by hackbio
-
-print "Name : Ishita Dasgupta\n";
-print "Email id : ishita2798\@gmail.com\n";
-print "Slack username : \@IshitaDasgupta\n";
-print "Biostack : Molecular Modelling, Drug Development, Data Analytics.\n";
