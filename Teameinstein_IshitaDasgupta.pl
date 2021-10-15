7711035bbc84444a1512e66fe7a0bca8b5c3af47
Ishita Dasgupta <84772358+ishitadasgupta@users.noreply.github.com>
2021-10-15 20:03:34 +0530

    Add files via upload

---
+++ Teameinstein_IshitaDasgupta.pl
@@ -0,0 +1,5 @@
+use strict;
+use warnings;
+
+#printing the required information
+print ("Ishita Dasgupta" , "\t ishita2798\@gmail.com" , "\t \@IshitaDasgupta" , "\t Molecular Modelling, Drug Development, Data Analytics.\n");
