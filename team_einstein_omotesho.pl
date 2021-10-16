6a3114d99e19bb965b7466b7526f9fe1be35839f
Namrata2801 <namrata.7838@gmail.com>
2021-10-16 09:14:20 +0530

    Added Quadri's output

--- team_einstein_omotesho.pl
+++
@@ -1,2 +0,0 @@
-
-print "Name ","- ","Omotesho Quadri\n","Email " ,"- " ,"omoteshoquadri@gmail.com\n","Discord_username " ,"- ","@","Quadri\n","Biostack ","- ","Genomic Medicine ","& ","Drug discovery";
